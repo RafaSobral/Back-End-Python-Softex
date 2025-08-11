25d2c1ee9195103df65f0a87c95ad89dc506ed3c
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2025-08-11 15:57:32 -0300

    Exercicios de PseudoCodigo e manipulações de String

---
+++ Aula03/exercicio2.py
@@ -0,0 +1,10 @@
+#---------------------------------------------------------------
+
+senha = input("Digite a sua senha:")
+
+if len(senha) < 8:
+    print("A senha deve ter mais que 8 caracteres")
+else:
+    print("Senha cadastrada")
+
+#---------------------------------------------------------------
---
+++ Aula03/exercicio3.py
@@ -0,0 +1,10 @@
+#---------------------------------------------------------------
+
+frase = input("Digite uma frase: ").lower()
+
+if "bomba" in frase:
+    print("A palavra bomba esta na frase")
+else:
+    print("A palavra bomba nao esta na frase")
+
+#---------------------------------------------------------------
