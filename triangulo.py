f84a599e3b7640e86ddd8e07d1d9c0bd0223f191
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2025-08-31 12:55:59 -0300

    Update triangulo.py

--- triangulo.py
+++ triangulo.py
@@ -1,54 +1,6 @@
-'''Seu objetivo: Escrever um algoritmo em Python que determine se três valores, 
-fornecidos pelo usuário, podem formar um triângulo.
 
-As Regras do Jogo
-1- Teste se a entrada de dados é um número.
-2- Se for um número teste se é positivo
-3- Para que três lados (lA,lB,lC) formem um triângulo, eles devem obedecer a duas condições importantes:
-
-A soma: A soma de quaisquer dois lados deve ser maior que o terceiro lado.
-
-lA<lB+lC
-
-lB<lA+lC
-
-lC<lA+lB
-
-A diferença: O valor absoluto da diferença entre dois lados deve ser menor que o terceiro lado.
-
-lA>∣lB−lC∣
-
-lB>∣lA−lC∣
-
-lC>∣lA−lB∣
-
-Dica: use o método abs() para ter o valor absoluto de um número.
-quero que vc me ajude a resolveer esse desafio'''
 
 print("Desafio do Triangulo: Vamos descobrir se os três números que você escolheu conseguem formar um triangulo?")
-
-'''while True:
-    ladoA = int(input("Digite o 1° número: "))
-    if ladoA <= 0:
-        print("Invalido digite um número inteiro!")
-        continue
-    ladoB = int(input("Digite o 2° número: "))
-    if ladoB <= 0:
-      print("Invalido digite um número inteiro!")
-      continue
-    ladoC = int(input("Digite o 3° númeoro: "))
-    if ladoC <= 0:
-        print("Invalido digite um número inteiro!")
-        break
-while True:
-    soma = (ladoA < ladoB + ladoC) and (ladoB < ladoA + ladoC) and (ladoC < ladoA + ladoB)
-
-    diferenca = (ladoA > abs(lB - lC)) and (lB > abs(lA - lC)) and (lC > abs(lA - lB))
-
-    if soma and diferenca:
-            print("Os valores podem formar um triângulo!")
-    else:
-        print("Entrada inválida! Digite apenas números.")'''
 
 n1 = input("Digite o primeiro valor: ")
 n2 = input("Digite o segundo valor: ")
@@ -69,6 +21,7 @@ if n1.isnumeric() and n2.isnumeric() and n3.isnumeric():
         print("Um dos numeros nao eh positivo")
 else:
     print("Um dos valores digitados nao eh um numero!")
+
 
 
 
