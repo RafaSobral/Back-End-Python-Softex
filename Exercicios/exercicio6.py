6d21aeb5462125e5a3483a055d29dc4d40926a37
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2025-11-05 21:49:29 -0300

    Exercicio 6

---
+++ Exercicios/exercicio6.py
@@ -0,0 +1,7 @@
+# Nível Intermediário 
+# Exercício 6: Crie um algoritmo que leia números do usuário ENQUANTO ele não digitar 0. 
+# Ao final, diga quantos números pares e quantos números ímpares foram digitados. 
+
+r = 1
+while(r != 0):
+    r = int(input("Digite um numero:"))
