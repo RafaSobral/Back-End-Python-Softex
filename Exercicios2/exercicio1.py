7f61909bbbc4e5477fc7f358f1a9b1bca94e388b
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2025-11-05 22:42:03 -0300

    exercicio1

---
+++ Exercicios2/exercicio1.py
@@ -0,0 +1,9 @@
+# Conversor de Moedas Simples (Variáveis e input): 
+# ○ Crie um programa que peça ao usuário o valor em reais (float). 
+# ○ Calcule o valor equivalente em dólar, sabendo que 1 dólar = R$ 5,00. 
+# ○ Imprima o resultado.
+
+v = float(input("Digite o valor: "))
+d = v / 5.00
+
+print(d)
