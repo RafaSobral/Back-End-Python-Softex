8f146b320a083c34c6b702ac5e049f0e56e66baf
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2025-11-05 21:48:10 -0300

    exercicio 2

---
+++ Exercicios/exercicio2.py
@@ -0,0 +1,7 @@
+# Nível Básico 
+# Exercício 2: Crie um algoritmo para somar dois números e mostrar o resultado.
+
+valor1 = int(input('Digite o primeiro valor: '))
+valor2 = int(input('Digite o segundo valor: '))
+
+print(f"A soma dos valores eh:", valor1 + valor2)
