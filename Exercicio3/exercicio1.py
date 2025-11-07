5dce0bc9e24b54867946bebdf1528410a6994e19
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2025-11-07 11:20:51 -0300

    exercicio 1

---
+++ Exercicio3/exercicio1.py
@@ -0,0 +1,10 @@
+# ; Exercício 1: Verificador de Positivo 
+# ; ● Peça ao usuário para digitar um número inteiro. 
+# ; ● Use uma estrutura if simples para verificar se o número é maior que zero. 
+# ; ● Se for, imprima "O número é positivo!". 
+
+n = int(input("Digite um numero inteiro: "))
+if n > 0:
+    print("O numero eh positivo")
+else:
+    print("O numero nao eh positivo")
