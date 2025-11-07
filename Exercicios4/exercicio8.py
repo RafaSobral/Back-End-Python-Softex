e3432712a7f92a93b9f2405a8a94b5730ce59213
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2025-11-07 19:43:53 -0300

    exercicio 9

---
+++ Exercicios4/exercicio8.py
@@ -0,0 +1,13 @@
+# Exercício 8: Buscando uma Palavra em uma Frase 
+# 1. Peça ao usuário que digite uma frase e guarde-a em uma variável. 
+# 2. Peça ao usuário que digite uma palavra e guarde-a em outra variável. 
+# 3. Use o operador in dentro de uma estrutura if para verificar se a palavra está contida na frase. 
+# 4. Exiba uma mensagem informando se a palavra foi encontrada ou não. 
+
+frase = input("Digite uma frase: ")
+palavra = input("Digite uma palavra: ")
+
+if palavra in frase:
+    print("A palavra esta na frase")
+else:
+    print("Ta nao men")
