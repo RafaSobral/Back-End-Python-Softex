17e45ea0b8b9b6896140c2c12945113b24b706a9
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2025-11-07 17:47:38 -0300

    exercicio 1

---
+++ Exercicios4/exercicio1.py
@@ -0,0 +1,10 @@
+# Exercício 1: Exibindo Itens de uma Lista 
+# 1. Crie uma variável chamada frutas e atribua a ela uma lista com 3 nomes de frutas 
+# (strings). 
+# 2. Use um loop for para percorrer cada item da lista frutas. 
+# 3. Dentro do loop, use o print() para exibir o nome de cada fruta em uma nova linha.
+
+frutas = ["laranja","abacate","uva"]
+
+for fruta in frutas:
+    print(fruta)
