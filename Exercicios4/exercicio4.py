7147a4f450bf4a8a887cfeadfb016394cae4e7c5
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2025-11-07 18:10:39 -0300

    exercicio 4

---
+++ Exercicios4/exercicio4.py
@@ -0,0 +1,11 @@
+# Exercício 4: Imprimindo uma Sequência de Números 
+# 1. Use um loop for e a função range() para gerar os números de 1 até 5. 
+# 2. Dentro do loop, use o print() para exibir cada número.
+
+m = range(1,10)
+
+for n in m:
+    print(n)
+
+for n in range (1,6):
+    print(n)
