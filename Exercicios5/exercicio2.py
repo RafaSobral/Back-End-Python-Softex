6f80a95b4a3f0eb839024dac9c08f6a620239e5a
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2025-11-07 21:20:36 -0300

    exercicio 2

---
+++ Exercicios5/exercicio2.py
@@ -0,0 +1,10 @@
+# Exercício 2 - Iniciante 
+# Faça um programa que solicita dois números e verifica se a multiplicação deles é igual a 100.
+
+n1 = int(input("Digite um numero"))
+n2 = int(input("Digite um numero"))
+
+if n1 * n2 == 100:
+    print("Eh igual a 100")
+else:
+    print("Nao eh igual!")
