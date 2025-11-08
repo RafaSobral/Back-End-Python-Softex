68ce9b95fd5e8fdd635db4b9b74fb5c64f205678
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2025-11-07 21:41:51 -0300

    exercicio 5

---
+++ Exercicios5/exercicio5.py
@@ -0,0 +1,11 @@
+# Exercício 5 - Iniciante 
+# Faça um programa que pede ao usuário para digitar uma string e, em seguida, imprime a 
+# string com todos os caracteres convertidos para maiúsculas e minúsculas alternadamente.
+
+palavra = input("Palavra: ")
+nova_palavra = ""
+
+for i in range(len(palavra)):
+   print(i)
+
+print(nova_palavra)
