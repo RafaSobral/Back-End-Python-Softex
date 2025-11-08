a84381bec1102b26f4d5793e0049e5d075d85faf
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2025-11-07 21:43:51 -0300

    exercicio6

---
+++ Exercicios5/exercicio6.py
@@ -0,0 +1,8 @@
+# Exercício 6 - Iniciante 
+# Escreva um programa que solicita uma palavra e verifica se ela tem a letra 'x'.
+
+palavra = input("Palavra: ")
+if "x" in palavra:
+    print("TEM")
+else:
+    print("Nao tem")
