a872705521d5a6762b6c9947296066d93ccb85f7
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2025-11-10 16:02:29 -0300

    Add files via upload

---
+++ Projeto Django/core/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Tarefa # 1. Importe o Model Tarefa
+def home(request):
+# 2. Use o ORM para buscar os dados!
+# Tarefa.objects.all() significa: "Pegue todas as linhas da tabela Tarefa"
+    todas_as_tarefas = Tarefa.objects.all()
+    # 3. Atualize o contexto
+    context = {
+    'nome_usuario': 'Júnior',
+    'tecnologias': ['Python', 'Django', 'Models', 'Admin'],
+    'tarefas': todas_as_tarefas # 4. Adicione as tarefas ao contexto
+    }
+    return render(request, 'home.html', context)
+
+
+def login(request):
+    return HttpResponse("<input>Login</input>")
